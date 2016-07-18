a99565553117e1f898a7bcf70436fff9aeca2644
Fernando <facr73@yahoo.com>
2016-07-18 23:04:58 +0100

    Create TAnk and TankPlayerController C++ files. Changed the Classes in BluePrints to this new C++ Classes

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+
+ATank* ATankPlayerController::GetControlledPawn() const {
+
+	return Cast<ATank>(GetPawn());
+
+}
+
+
+
+
